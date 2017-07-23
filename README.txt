8bd2d49a0f9ffb891ab435dbc5552511ed8f50b0
Anastasiya Babkova <anastasiya_babkova@epam.com>
2017-07-23 22:30:12 +0300

    Added latest page and added push state

---
+++ README.txt
@@ -0,0 +1,8 @@
+install node https://nodejs.org/en/ 6.x version
+install yarn https://yarnpkg.com/en/docs/install
+
+run commands:
+
+yarn
+
+yarn dev
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+function initScripts() {
     $('.slick-carousel').slick({
         vertical: true,
         verticalSwiping: true,
@@ -9,11 +9,33 @@ $(document).ready(function(){
         autoplaySpeed: 3000,
     });
 
-    $('.toggler').click(function(){
-        $(this).toggleClass('open');
-        $('#main-menu').toggleClass('open');
-    });
+    AOS.refresh();
+}
 
+function destroyScripts() {
+    $('.slick-carousel').slick('unslick');
+}
+function getCurrentPageName() {
+    var newPageArray = location.pathname.split('/');
+    return newPageArray[newPageArray.length - 1];
+}
+
+function getBodyClass(url) {
+    return 'cd-' + url.replace('.html', '');
+}
+
+var TRANSITIONS = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
+var $MAIN_CONTAINER = $('main');
+var DELAY = 500;
+var $BODY = $('body');
+var CHANGING_PAGE_CLASS = 'page-is-changing';
+var $LINK = $('[data-type="page-transition"]');
+
+$(document).ready(function () {
+    $('.toggler, .overlay').click(function(){
+        $('.toggler').toggleClass('open');
+        $('.page').toggleClass('open');
+    });
     AOS.init({
         offset: 200,
         duration: 600,
@@ -21,40 +43,60 @@ $(document).ready(function(){
         delay: 100,
     });
 
-    // Select all links with hashes
-    $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 500, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
-                        };
-                    });
-                }
+    initScripts();
+
+    var firstLoad = false;
+
+    $BODY.addClass(getBodyClass(getCurrentPageName()));
+
+    //trigger smooth transition from the actual page to the new one
+    $LINK.on('click', function (event) {
+        event.preventDefault();
+        //if the page is not already being animated - trigger animation
+        changePage($(this).attr('href'), true);
+        firstLoad = true;
+    });
+
+    //detect the 'popstate' event - e.g. user clicking the back button
+    $(window).on('popstate', function () {
+        if (firstLoad) {
+            var newPage = getCurrentPageName();
+            changePage(newPage, false);
+        }
+        firstLoad = true;
+    });
+
+    function changePage(url, bool) {
+
+        // trigger page animation
+        $BODY.addClass(CHANGING_PAGE_CLASS);
+        $('.cd-loading-bar').one(TRANSITIONS, function () {
+            loadNewContent(url, bool);
+            $('.cd-loading-bar').off(TRANSITIONS);
+        });
+    }
+
+    function loadNewContent(url, bool) {
+        url = (!url) ? 'index.html' : url;
+        var newSection = getBodyClass(url),
+            section = $('<div class="cd-main-content ' + newSection + '"></div>');
+
+        section.load(url + ' .cd-main-content > *', function () {
+            setTimeout(function () {
+                $BODY.removeClass().addClass(newSection);
+                $('.cd-loading-bar').one(TRANSITIONS, function () {
+                    $('.cd-loading-bar').off(TRANSITIONS);
+                });
+
+                destroyScripts();
+                $MAIN_CONTAINER.html(section);
+                initScripts();
+
+            }, DELAY);
+
+            if (url !== window.location && bool) {
+                window.history.pushState({path: url}, '', url);
             }
         });
+    }
 });
